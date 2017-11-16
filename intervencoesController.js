42069017d38bd85d004372773647eb3a9dc86051
BVMDev <bru.vmartins@gmail.com>
2017-11-15 23:29:40 -0200

    Melhorias
    Melhorias

---
+++ intervencoesController.js
@@ -0,0 +1,78 @@
+"USE STRICT";
+app.controller("intervencoesController", function($scope, $location, dbService){
+
+	$scope.status = ["TO DO", "DOING", "WAITING", "DONE"];
+	$scope.sistema = ["ON6", "AH5", "OO3", "CX7","XD","VJ","VX","OK2","OK4","OK5","Y9","Y6","PL","OT5","LD"];
+
+	//Listando
+	$scope.lista = function(){
+		dbService.runAsync("SELECT * FROM intervencoes", function(data){
+			$scope.interv = data;
+		});
+	}
+
+	//Salvando
+	$scope.salvar = function(){
+		if($scope.int.id){
+			//Editar
+		
+			var id = $scope.int.id;
+			delete $scope.int.id;
+			delete $scope.int.$$hashKey; //Apaga elemento $$hashKey do objeto
+			dbService.update('intervencoes', $scope.int, {id: id}); //entidade, dados, where
+		}else{
+			//nova
+			dbService.insert('intervencoes', $scope.int); // entidade, dados
+		}
+		$scope.interv = {};
+		$scope.lista();
+		$('#modalInterv').modal('hide');
+	}
+
+	//Abrindo para editar
+	$scope.editar = function(dados){
+		$scope.int = dados;
+		$('#modalInterv').modal('show');
+	}
+
+
+	$scope.showDescricao = function(dados){
+		$scope.int = dados;
+		dbService.runAsync("SELECT * FROM log where id_intervencao = "+ dados.id + " order by data desc", function(data){
+			$scope.intervLog = data;		
+		});
+		$('#modalIntervDesc').modal('show');
+	}
+
+	//Abrindo LOG
+	$scope.inserirLogs = function(dados){	
+		$scope.int = dados;
+		$scope.intLog = {"data":"", "log":"", "id_intervencao" : dados.id };
+		$('#modalIntervLog').modal('show');
+	}
+
+	$scope.inserirLog = function(dados){		
+		dbService.insert('log', dados); // entidade, dados
+		$('#modalIntervLog').modal('hide');
+	}
+
+//Excluindo
+$scope.excluirLog = function(dados){
+	if(confirm("Deseja realmente apagar o log ?")){
+		//dbService.delete('intervencoes', {id: dados.id});
+		dbService.run("DELETE FROM log WHERE id = ?;",[dados.id]);
+		dbService.runAsync("SELECT * FROM log where id_intervencao = "+ dados.id_intervencao + " order by data desc", function(data){
+			$scope.intervLog = data;			
+		});
+	}
+}
+	//Excluindo
+	$scope.excluir = function(dados){
+		if(confirm("Deseja realmente apagar o cadastro de "+dados.id+"?")){
+			//dbService.delete('intervencoes', {id: dados.id});
+			dbService.run("DELETE FROM intervencoes WHERE id = ?;",[dados.id]);
+			$scope.lista();
+		}
+		
+	}
+});
